2b85fea68b3a76f77e2cbd413d54bcc96e383f05
varun krishna korukondabattar <korukondabattarvarunkrishna@gmail.com>
2026-01-02 05:44:56 +0530

    Add files via upload

---
+++ python introduction.py
@@ -0,0 +1,60 @@
+'''a= 100
+b = 1.5
+print(type(a))
+print(type(b))'''
+
+'''num = 5+ 2.0
+
+print(num)
+print(type(num))'''
+
+
+'''num_int = int(5.7)
+
+print(num_int)
+print(type(num_int))
+
+num_float = float("3.14")
+
+print(num_float)
+print(type(num_float))
+
+message = str(42)
+
+print(message)
+print(type(message))
+
+is_true = bool(1)
+is_false = bool(0)
+
+print(is_true)
+print(is_false)'''
+
+
+
+'''a = "john"
+b= int(a)
+
+print(b)'''
+
+#a = 65
+#print(chr(a))
+
+
+'''print(3//2)
+print(10%3)
+print(2**3)'''
+
+
+''''a=10
+b =20
+
+a/=b
+print(a)'''
+
+
+##print("a")
+
+
+
+
