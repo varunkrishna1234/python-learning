8958626823e9bcfff0b7f2397edcef3158f6e2b0
varun krishna korukondabattar <korukondabattarvarunkrishna@gmail.com>
2026-01-04 07:56:56 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,99 @@
+'''a= input(" whats your name ?")
+
+print(a)'''
+
+
+'''number = int(input("give me number "))
+
+print(type(number))'''
+
+'''name = input(" whats your name = ")
+age = int (input(" whats your age = "))
+
+
+print("hello",name,"! , your age is ",age)'''
+
+
+
+#example 1
+#input john output hello,john 
+
+'''name = input("whats your name = ")
+
+print("Hello,",name)'''
+
+
+'''number = int(input("whats the number = "))
+
+
+print("You entered:",number)'''
+
+
+'''number = float(input())
+
+print("valuse of: ",number)
+'''
+
+'''number = input("enter the numbers ")
+
+x,y,z = number.split(" ")
+sum = int(x) + int(y) + int(z)
+
+print(sum)
+'''
+
+'''inp = input("enter the name and age = ")
+
+name,age = inp.split(",")
+
+print("my name is : ",name," and i am",age,"years old")
+
+'''
+
+
+'''
+a = input("Enter the number = ")
+
+x,y= a.split(" ")
+print(int(x)+int(y))
+'''
+'''
+r = int(input(" enter the number = "))
+
+a = 3.14 *(r*r)
+print(a)
+'''
+'''
+a = int(input("enter number A ="))
+b = int(input("enter number B ="))
+c = int(input("enter number C ="))
+root1=0
+root2=0
+d= (b**2) - 4*a*c
+
+root1= (-b +(d**(0.5)))/2*a
+
+root2= (-b -(d**(0.5)))/2*a
+
+print(f"Root:({root1},{root2})")
+'''
+'''
+a = 10
+b = 20 
+temp =a
+a=b
+b = temp
+print("A= ",a,"B = ",b)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
