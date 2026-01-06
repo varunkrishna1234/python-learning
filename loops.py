cc89b456a4a424448751301dcfe9eb75bbde3d39
varun krishna korukondabattar <korukondabattarvarunkrishna@gmail.com>
2026-01-06 17:14:26 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,122 @@
+#loops 
+
+'''loops are used to excute a block of code 
+repeatedlly as long as a certain condition 
+is true or for a specific number of itereations'''
+
+#types
+
+#while
+#for
+
+
+#while loop
+
+''' the  while loop excutes a block of code as long as
+specied condition is true.it continously checks the 
+condition before each iteration and stops when 
+the condition becomes false''' 
+
+'''candies = 10
+
+while candies>0:
+    print("given to friend")
+    candies -=1
+
+print(candies)  ''' 
+
+
+#for loop
+'''A for loop is a way repeat a block of code for each item 
+in a collection or for a specific range of numbers'''
+
+#syntax
+#for variable in range(start,stop,step):
+
+'''candies =10
+
+for i in range(candies):
+    print("given to friend")'''
+
+
+'''message = "Hello,World"
+
+#for i in message:
+ #   print(i)
+
+length = len(message)
+for i in range(length):
+    print(i)'''
+
+
+
+'''for i in range(2,3):
+    for j in range(1,11):
+        print(f"{i} X {j} = {i*j}")'''
+
+
+#print number from 1 to N
+
+#N= int(input("Enter the number = "))
+'''i =1
+
+while i<=N:
+    print(i)
+    i +=1'''
+
+
+#for i in range(1,N+1):
+ #   print(i)
+
+
+#calcuate the sum of N natural number
+
+'''n = int(input("Enter the number = "))
+
+i= 1
+sum =0
+while i<=n:
+    sum =i +sum
+    i +=1
+    
+print(sum)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
